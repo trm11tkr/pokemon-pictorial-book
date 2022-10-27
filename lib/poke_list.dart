dc4b833ae6cd9d6b8bfafbe574afc631f3522c7e
森田 陸斗 <r_morita@MacBook-Pro.local>
2022-10-27 19:02:12 +0900

    update design of switch the fav mode

--- lib/poke_list.dart
+++ lib/poke_list.dart
@@ -55,27 +55,166 @@ class _PokeListState extends State<PokeList> {
     }
   }
 
+  void changeFavMode(bool currentMode) {
+    setState(() => isFavoriteMode = !currentMode);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Consumer<PokemonNotifier>(
-      builder: (context, pokes, child) => ListView.builder(
-          padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
-          itemCount: itemCount(_currentPage) + 1,
-          itemBuilder: (context, index) {
-            if (index == itemCount(_currentPage)) {
-              return OutlinedButton(
-                  onPressed: !isLastPage(_currentPage)
-                      ? () => {
-                            setState(() => _currentPage++),
-                          }
-                      : null,
-                  child: const Text('more'));
-            } else {
-              return PokeListItem(
-                pokemon: pokes.byId(itemId(index)),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        Container(
+          height: 24,
+          alignment: Alignment.topRight,
+          child: IconButton(
+            padding: const EdgeInsets.all(0),
+            icon: const Icon(Icons.auto_awesome_outlined),
+            onPressed: () async {
+              var ret = await showModalBottomSheet<bool>(
+                context: context,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40),
+                  ),
+                ),
+                builder: (BuildContext context) {
+                  return ViewModeBottomSheet(
+                    favMode: isFavoriteMode,
+                    changeFavMode: changeFavMode,
+                  );
+                },
               );
-            }
-          }),
+              if (ret != null && ret) {
+                changeFavMode(isFavoriteMode);
+              }
+            },
+          ),
+        ),
+        Expanded(
+          child: Consumer<PokemonNotifier>(
+            builder: (context, pokes, child) => ListView.builder(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+                itemCount: itemCount(_currentPage) + 1,
+                itemBuilder: (context, index) {
+                  if (index == itemCount(_currentPage)) {
+                    return OutlinedButton(
+                        onPressed: !isLastPage(_currentPage)
+                            ? () => {
+                                  setState(() => _currentPage++),
+                                }
+                            : null,
+                        child: const Text('more'));
+                  } else {
+                    return PokeListItem(
+                      pokemon: pokes.byId(itemId(index)),
+                    );
+                  }
+                }),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ViewModeBottomSheet extends StatelessWidget {
+  const ViewModeBottomSheet({
+    Key? key,
+    required this.favMode,
+    required this.changeFavMode,
+  }) : super(key: key);
+  final bool favMode;
+  final Function(bool) changeFavMode;
+
+  String mainText(bool fav) {
+    return '表示設定';
+  }
+
+  String menuFavTitle(bool fav) {
+    if (fav) {
+      return '「すべて」表示に切り替え';
+    } else {
+      return '「お気に入り」表示に切り替え';
+    }
+  }
+
+  String menuFavSubtitle(bool fav) {
+    if (fav) {
+      return '全てのポケモンが表示されます';
+    } else {
+      return 'お気に入りに登録したポケモンのみが表示されます';
+    }
+  }
+
+  String menuGridTitle(bool grid) {
+    if (grid) {
+      return 'リスト表示に切り替え';
+    } else {
+      return 'グリッド表示に切り替え';
+    }
+  }
+
+  String menuGridSubtitle(bool grid) {
+    if (grid) {
+      return 'ポケモンをグリッド表示します';
+    } else {
+      return 'ポケモンをリスト表示します';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: Center(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 5,
+              width: 30,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Theme.of(context).backgroundColor,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+              child: Text(
+                mainText(favMode),
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.swap_horiz),
+              title: Text(
+                menuFavTitle(favMode),
+              ),
+              subtitle: Text(
+                menuFavSubtitle(favMode),
+              ),
+              onTap: () {
+                changeFavMode(favMode);
+                Navigator.pop(context);
+              },
+            ),
+            OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(40)),
+              ),
+              child: const Text('キャンセル'),
+              onPressed: () => Navigator.pop(context, false),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
