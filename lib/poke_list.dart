ed16e5082c5ba29ca068ff3aeb247cde0efeac74
森田 陸斗 <r_morita@MacBook-Pro.local>
2022-10-27 18:13:30 +0900

    support pagination

--- lib/poke_list.dart
+++ lib/poke_list.dart
@@ -1,20 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:pokemon_pictorial_book/const/poke_api.dart';
 import 'package:pokemon_pictorial_book/models/pokemon.dart';
 import 'package:pokemon_pictorial_book/poke_list_item.dart';
 import 'package:provider/provider.dart';
 
-class PokeList extends StatelessWidget {
+class PokeList extends StatefulWidget {
   const PokeList({super.key});
 
+  @override
+  State<PokeList> createState() => _PokeListState();
+}
+
+class _PokeListState extends State<PokeList> {
+  static const int more = 30;
+  int pokeCount = more;
   @override
   Widget build(BuildContext context) {
     return Consumer<PokemonNotifier>(
       builder: (context, pokes, child) => ListView.builder(
-        padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
-        itemCount: 10,
-        itemBuilder: (context, index) =>
-            PokeListItem(pokemon: pokes.byId(index + 1)),
-      ),
+          padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+          itemCount: pokeCount + 1,
+          itemBuilder: (context, index) {
+            if (index == pokeCount) {
+              return OutlinedButton(
+                child: const Text('more'),
+                onPressed: () => {
+                  setState(
+                    () {
+                      pokeCount = pokeCount + more;
+                      if (pokeCount > pokeMaxId) {
+                        pokeCount = pokeMaxId;
+                      }
+                    },
+                  )
+                },
+              );
+            } else {
+              return PokeListItem(
+                pokemon: pokes.byId(index + 1),
+              );
+            }
+          }),
     );
   }
 }
